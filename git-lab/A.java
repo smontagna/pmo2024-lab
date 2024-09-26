1a41d7d2777c0ca8b1c1080df83433dc441ff93d
Sara Montagna <sara.montagna@unibo.it>
2024-09-26 15:25:48 +0200

    add sum

--- git-lab/A.java
+++ git-lab/A.java
@@ -1,7 +1,8 @@
 class A{
-    
+
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Fai la somma tra 2 e 1 = " + B.sum(2,1));
     }
 
 }

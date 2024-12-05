8b05e12c77e1088856c77798287490f88a8e9fee
Sara Montagna <sara.montagna@unibo.it>
2024-12-05 12:28:33 +0100

    feat: add lab 7

---
+++ lab7/it/unibo/oop/lab/gui1/GUI.java
@@ -0,0 +1,13 @@
+package it.unibo.oop.lab.gui1;
+
+
+import javax.swing.*;
+
+
+public class GUI extends JFrame{
+	
+	public GUI(int size){
+		
+	}
+
+}
---
+++ lab7/it/unibo/oop/lab/gui2/GUI.java
@@ -0,0 +1,13 @@
+package it.unibo.oop.lab.gui2;
+
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class GUI extends JFrame {
+    
+    public GUI(int size) {
+    }
+    
+}
